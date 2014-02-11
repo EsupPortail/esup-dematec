7ecdacf370753bd7cc7edc9371df949d3c0eb453
Vincent Bonamy <vbonamy@gmail.com>
2014-02-11 10:49:35 +0100

    Archive : metada.csv

--- src/main/java/fr/univrouen/poste/services/ArchiveService.java
+++ src/main/java/fr/univrouen/poste/services/ArchiveService.java
@@ -19,18 +19,28 @@ package fr.univrouen.poste.services;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
-import java.util.zip.ZipEntry;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.supercsv.cellprocessor.FmtBool;
+import org.supercsv.cellprocessor.FmtDate;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.constraint.LMinMax;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.constraint.UniqueHashCode;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import fr.univrouen.poste.domain.PosteCandidature;
 import fr.univrouen.poste.domain.PosteCandidatureFile;
@@ -49,6 +59,9 @@ public class ArchiveService {
 		File destFolderFile = new File(destFolder);
 		if(destFolderFile.mkdir()) {
 	
+			final String[] header = new String[] { "id", "filename", "sendDate", "owner" };
+			final CellProcessor[] processors = getProcessors();
+			
 			for (PosteCandidature posteCandidature : posteCandidatures) {
 				String folderName = destFolder.concat("/");
 				folderName = folderName.concat(posteCandidature.getPoste().getNumEmploi().concat("/"));	
@@ -62,6 +75,9 @@ public class ArchiveService {
 				
 				folder = new File(folderName);
 				folder.mkdir();
+				
+				ICsvBeanWriter beanWriter =  new CsvBeanWriter(new FileWriter(folderName.concat("metadata.csv")), CsvPreference.STANDARD_PREFERENCE);
+				beanWriter.writeHeader(header);
 
 				for (PosteCandidatureFile posteCandidatureFile : posteCandidature.getCandidatureFiles()) {
 					String fileName = posteCandidatureFile.getId().toString().concat("-").concat(posteCandidatureFile.getFilename());
@@ -72,10 +88,65 @@ public class ArchiveService {
 					OutputStream outputStream = new FileOutputStream(file);
 					InputStream inputStream = posteCandidatureFile.getBigFile().getBinaryFile().getBinaryStream();					
 					IOUtils.copyLarge(inputStream, outputStream);
+					
+					ArchiveMetadataFileBean archiveMetadataFileBean = new ArchiveMetadataFileBean(fileName, posteCandidatureFile.getFilename(), posteCandidatureFile.getSendTime(), posteCandidature.getCandidat().getEmailAddress());
+					beanWriter.write(archiveMetadataFileBean, header, processors);
 				}
+				
+				beanWriter.close();
 			}
 		
 		}
+	}
+	
+	
+	public class ArchiveMetadataFileBean {
+		
+		String id;
+		
+		String filename;
+		
+		Date sendDate;
+		
+		String owner;
+
+		public ArchiveMetadataFileBean(String id, String filename,
+				Date sendDate, String owner) {
+			super();
+			this.id = id;
+			this.filename = filename;
+			this.sendDate = sendDate;
+			this.owner = owner;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public String getFilename() {
+			return filename;
+		}
+
+		public Date getSendDate() {
+			return sendDate;
+		}
+
+		public String getOwner() {
+			return owner;
+		}
+		
+	}
+
+	private static CellProcessor[] getProcessors() {
+
+		final CellProcessor[] processors = new CellProcessor[] { 
+				new UniqueHashCode(), // id (must be unique)
+				new NotNull(), // filename
+				new FmtDate("dd/MM/yyyy - HH:mm"), // sendTime
+				new NotNull(), // owner
+		};
+
+		return processors;
 	}
 	
 }
