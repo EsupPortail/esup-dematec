2e6bcf29624d50aa73cb1997a26f08bb052b0ab1
Vincent Bonamy <vbonamy@gmail.com>
2014-02-19 10:56:49 +0100

    Password : longueur minimum : 6 caractères

---
+++ src/main/webapp/js/demat.js
@@ -0,0 +1,14 @@
+    $(document).ready(function() {  	
+    	
+    	
+    	// Verification longueur min du password 
+    	$("#changePasswordForm").submit(function( event ) {  	
+			var newPasswd = $('#_changepassword_newpassword');
+			if (newPasswd.val().length < 6) {
+				alert('Merci de saisir un password contenant au moins 6 caractères.');
+				 event.preventDefault();
+			} 
+    	});  
+    });  
+    
+    
