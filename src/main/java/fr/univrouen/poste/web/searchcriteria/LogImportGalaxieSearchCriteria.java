a923c150d0a60754d09caae7a6faa3cc10100c8e
Vincent Bonamy <vbonamy@gmail.com>
2014-10-21 11:12:52 +0200

    Improve LogImportGalaxieController

---
+++ src/main/java/fr/univrouen/poste/web/searchcriteria/LogImportGalaxieSearchCriteria.java
@@ -0,0 +1,10 @@
+package fr.univrouen.poste.web.searchcriteria;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+
+@RooJavaBean
+public class LogImportGalaxieSearchCriteria {
+
+	String status;
+	
+}
