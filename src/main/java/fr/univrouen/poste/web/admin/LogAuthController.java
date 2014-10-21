bd2d02ee7a87f1d04f5c3f73a1f0e0e1e0f219be
Vincent Bonamy <vbonamy@gmail.com>
2014-10-21 11:38:28 +0200

    Improve LogAuthController

--- src/main/java/fr/univrouen/poste/web/admin/LogAuthController.java
+++ src/main/java/fr/univrouen/poste/web/admin/LogAuthController.java
@@ -17,20 +17,47 @@
  */
 package fr.univrouen.poste.web.admin;
 
-import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.univrouen.poste.domain.LogAuth;
+import fr.univrouen.poste.web.searchcriteria.LogImportGalaxieSearchCriteria;
 
 @RequestMapping("/admin/logauths")
 @Controller
 @RooWebScaffold(path = "admin/logauths", formBackingObject = LogAuth.class, create=false, update=false)
-@RooWebFinder
 public class LogAuthController {
-
+	
+    @ModelAttribute("command") 
+    public LogImportGalaxieSearchCriteria getLogSearchCriteria() {
+    	return new LogImportGalaxieSearchCriteria();
+    }
+    
+    @RequestMapping(params = "find=ByActionEquals", method = RequestMethod.GET)
+    public String findLogAuthsByActionEquals(@ModelAttribute("command") LogImportGalaxieSearchCriteria searchCriteria, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+    	if("".equals(searchCriteria.getStatus())) {
+    		return this.list(page, size, sortFieldName, sortOrder, uiModel);
+    	}
+    	if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("logauths", LogAuth.findLogAuthsByActionEquals(searchCriteria.getStatus(), sortFieldName, sortOrder).setFirstResult(firstResult).setMaxResults(sizeNo).getResultList());
+            float nrOfPages = (float) LogAuth.countFindLogAuthsByActionEquals(searchCriteria.getStatus()) / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("logauths", LogAuth.findLogAuthsByActionEquals(searchCriteria.getStatus(), sortFieldName, sortOrder).getResultList());
+        }    
+        
+        uiModel.addAttribute("command", searchCriteria);
+        uiModel.addAttribute("finderview", true);
+        
+        addDateTimeFormatPatterns(uiModel);
+        return "admin/logauths/list";
+    }
+    
 }
