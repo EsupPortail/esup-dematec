575fe7bba7fd1175f69d952ba21b49e4307b16a2
Vincent Bonamy <vbonamy@gmail.com>
2014-10-30 08:47:39 +0100

    Oups: appli_config_file_type dans import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -159,6 +159,10 @@ true
 
 );
 
+insert into appli_config_file_type (id, type_title, type_description, candidature_file_mo_size_max, candidature_nb_file_max, candidature_content_type_restriction_regexp, candidature_filename_restriction_regexp, version) 
+values (1, 'Fichier de candidature', 'Merci de sélectionner un fichier puis de valider votre sélection pour l''envoi effectif.', -1, -1, '.*', '.*', 1);
+
+
 insert into galaxie_mapping(id, id_email, id_num_candidat, id_numemploi, id_civilite, id_localisation, id_nom, id_prenom, id_profil, version) 
 values (1, 'Email', 'N° candidat', 'N° emploi', 'Civilité', 'Localisation', 'Nom', 'Prénom', 'Profil J.o', 0);
 
