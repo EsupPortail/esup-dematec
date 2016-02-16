f8ce11c6dd80aa3280049d9592bfad245be64255
Vincent Bonamy <vbonamy@gmail.com>
2016-02-16 14:14:09 +0100

    Affichage version EsupDematEC pour admins/super-manager

--- src/main/java/fr/univrouen/poste/domain/AppliVersion.java
+++ src/main/java/fr/univrouen/poste/domain/AppliVersion.java
@@ -1,5 +1,7 @@
 package fr.univrouen.poste.domain;
 
+import java.util.List;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -9,6 +11,22 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooJpaActiveRecord
 public class AppliVersion {
 
+	private static String cacheVersion;
+	
 	String esupDematEcVersion;
+	
+
+	public static String getCacheVersion() {
+		if(cacheVersion == null) {
+			AppliVersion appliVersion = null;
+			List<AppliVersion> appliVersions = AppliVersion.findAllAppliVersions();
+			if(appliVersions.isEmpty()) {
+				cacheVersion = "?!";
+			} else {
+				cacheVersion = appliVersions.get(0).getEsupDematEcVersion();
+			}
+		}
+		return cacheVersion;
+	}
 
 }
